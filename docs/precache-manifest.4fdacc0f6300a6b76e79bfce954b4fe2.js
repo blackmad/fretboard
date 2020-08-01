f4130f8377eca16cc97ad8b7987667dfb1f4c749
David Blackman <david@whizziwig.com>
2020-08-01 19:36:40 -0400

    make sure public gets commited

---
+++ docs/precache-manifest.4fdacc0f6300a6b76e79bfce954b4fe2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bb6a88f7bb7a22f5a323cf6d862fd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4822af2d9aa38bf71c4",
+    "url": "/static/css/main.fcbd8f9a.chunk.css"
+  },
+  {
+    "revision": "c9857dcdcdc30eb9c100",
+    "url": "/static/js/2.1a3828e7.chunk.js"
+  },
+  {
+    "revision": "023c37ab9b1c944f7c1c0a97be953446",
+    "url": "/static/js/2.1a3828e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4822af2d9aa38bf71c4",
+    "url": "/static/js/main.aece6176.chunk.js"
+  },
+  {
+    "revision": "5be44404da9e666587fa",
+    "url": "/static/js/runtime-main.21e39363.js"
+  },
+  {
+    "revision": "32941d6330044744c02493835b799e90",
+    "url": "/static/media/glyphicons-halflings-regular.32941d63.svg"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  }
+]);
