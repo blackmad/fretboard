b06a5fb774d6f2953d2fea3b0d035b2a1a83c453
David Blackman <blackmad@radar.io>
2021-01-07 23:05:40 -0500

    new css/js

---
+++ docs/precache-manifest.66166f89a708c7a7796cdba75abe3560.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6208ceeaa402833aa189ac077fd6fced",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94afcc00b592f496f420",
+    "url": "./static/css/main.66939a63.chunk.css"
+  },
+  {
+    "revision": "46fc615ed6b729525e8b",
+    "url": "./static/js/2.7eb4d2dd.chunk.js"
+  },
+  {
+    "revision": "023c37ab9b1c944f7c1c0a97be953446",
+    "url": "./static/js/2.7eb4d2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94afcc00b592f496f420",
+    "url": "./static/js/main.c144512e.chunk.js"
+  },
+  {
+    "revision": "06e3b71a78adb212b370",
+    "url": "./static/js/runtime-main.20bfbe4c.js"
+  },
+  {
+    "revision": "32941d6330044744c02493835b799e90",
+    "url": "./static/media/glyphicons-halflings-regular.32941d63.svg"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "./static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "./static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "./static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  }
+]);
